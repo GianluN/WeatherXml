f488980fcad173eab73bb8ecf8888deb5ab9b7aa
Gianluca Nuzzo <gianlu.nuzzo@gmail.com>
2024-03-26 15:40:02 +0100

    Update Constants.kt

--- app/src/main/java/com/example/provatecnica/utils/Constants.kt
+++ app/src/main/java/com/example/provatecnica/utils/Constants.kt
@@ -5,7 +5,7 @@ class Constants {
         const val KEY_TITLE = "title"
         const val KEY_URL = "url"
         const val KEY_IMAGE_URL = "image_url"
-        const val API_TOKEN = "lvyQAnmpvcHfiJc0ELd1lh2czUbpz1e3cPWliIDV2222"
+        const val API_TOKEN = 
         const val BASE_URL = "https://api.thenewsapi.com/v1/"
     }
 }
